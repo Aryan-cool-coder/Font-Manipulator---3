c3c42021630bc793fdbdc1e2a5d603c34a762818
Aryan-cool-coder <79555201+Aryan-cool-coder@users.noreply.github.com>
2022-01-04 17:16:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+difference =0;
+
+function preload() {
+
+}
+
+function draw() {
+
+}
+
+function setup() {
+video = createCapture(VIDEO);
+video.size(500, 500);
+canvas = createCanvas(500, 500);
+canvas.position(600, 150);
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    }
+} 
+
+function modelLoaded() {
+    console.log('Pose net is initialized');
+}
+
+function draw() {
+    background("navy");
+    textSize(difference);
+    fill('cyan');
+    text('Aryan', 50, 100);
+}
